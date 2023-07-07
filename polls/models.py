d6532d942b6114fdfe773441b53c6ef5138b2279
eyal <narnavg@gmail.com>
2023-07-07 11:23:25 +0300

    added readme + change to models

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -8,7 +10,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(minutes=1)
+    
 class Choice(models.Model): 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
---
+++ readme.md
@@ -0,0 +1,8 @@
+# clone instructions
+1. open terminal
+2. cd ..
+3. git clone https://github.com/ranerlich7/django_polls.git
+4. in visual studio go to File->open folder->open django_polls
+5. python -m venv venv
+6. ./venv/Scripts/activate
+7. pip install -r requirements.txt
