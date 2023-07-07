f656dcd171a8c776296f5efe626b6861d2868727
eyal <narnavg@gmail.com>
2023-07-07 13:06:14 +0300

    added admin and some view implementations

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 
+from polls.models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, Ran! You're at the polls index.")
+    all_questions = Question.objects.all()[0]
+    return HttpResponse(f"Hello, Ran! You're at the polls index.\n Thess are the questions:\n{all_questions}")
 
 def create(request):
+    
     return HttpResponse("POLL CREATED.")
 
 def delete(request):
