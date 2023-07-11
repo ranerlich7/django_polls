dc6f89d2ee008d9e0bb3701756237436a5d0eaf7
--global <ranerlich@gmail.com>
2023-07-11 20:39:55 +0300

    added endpoints for votes and questions

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
     path("delete/", views.delete, name="delete"),
+    # ex: /polls/5/
+    path("<int:question_id>", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:choice_id>/vote/", views.vote, name="vote"),
+        
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 
-from polls.models import Question
+from polls.models import Choice, Question
 
 
 def index(request):
-    all_questions = Question.objects.all()[0]
-    return HttpResponse(f"Hello, Ran! You're at the polls index.\n Thess are the questions:\n{all_questions}")
+    first_question = Question.objects.all()[0]
+    return HttpResponse(f"Hello, Ran! You're at the polls index.\n Thess are the questions:\n{first_question}")
 
 def create(request):
     
@@ -13,3 +13,27 @@ def create(request):
 
 def delete(request):
     return HttpResponse("POLL DELETED")
+
+
+def detail(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    print(question)
+    # details on the question. 
+    return HttpResponse(f"You're looking at question id{question_id} : {question}")
+
+
+def results(request, question_id):
+    # get all choices from question. choice_set
+    response = "You're looking at the results of question %s."
+    
+    # output = ", ".join([q.question_text for q in latest_question_list])
+
+    return HttpResponse(response % question_id)
+
+
+def vote(request, choice_id): # choiceID
+    choice = Choice.objects.get(pk=choice_id)    
+    choice.votes += 1
+    choice.save()
+    return HttpResponse("You're voting recieved on choice %s." % choice_id)
+
